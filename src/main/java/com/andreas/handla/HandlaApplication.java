459919c15349c95debbe0bfcc781a01da72e4096
anga95 <andreas.gabrielsson@live.se>
2023-09-09 14:32:27 +0200

    init

---
+++ src/main/java/com/andreas/handla/HandlaApplication.java
@@ -0,0 +1,13 @@
+package com.andreas.handla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandlaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andreas/handla/HandlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreas.handla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
